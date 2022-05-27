5d94a0a0d37b137d1a6820ee2f66918050d2fa88
sebcampos <sebcampos@criptext.com>
2022-05-27 16:53:31 -0700

    testing camera functions

---
+++ Testing.py
@@ -0,0 +1,27 @@
+import cv2
+import uuid   # allows us to give unique identifiers
+import os
+import numpy as np
+import time
+from DatabaseManagers import MongoDBClient
+
+labels = ["panda", "milo", "bubby"]
+
+
+
+def capture_image(name="bubby"):
+	cap = cv2.VideoCapture(0)
+	time.sleep(5)
+	ret, frame = cap.read()
+	MongoDBClient.add_image("ObjectDetection", convert_binary(frame), name)
+	cv2.imshow("frame", frame)
+	time.sleep(2)
+	
+ 
+ 
+def convert_binary(frame):
+	return cv2.imencode(".jpg", frame)[1].tostring()
+ 	
+def convert_image(binary):
+	# nparr = np.fromstring(binary, np.uint8)
+ 	return cv2.imdecode(binary, cv2.CV_LOAD_IMAGE_COLOR)	
