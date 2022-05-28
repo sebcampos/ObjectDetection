3fbf17163f9995ef36f5c28384a4a011998450cc
sebcampos <sebcampos@criptext.com>
2022-05-28 10:18:17 -0700

    updating gitignore

--- Testing.py
+++ Testing.py
@@ -3,7 +3,6 @@ import uuid   # allows us to give unique identifiers
 import os
 import numpy as np
 import time
-import atexit
 from DatabaseManagers import MongoDBClient
 
 labels = ["panda", "milo", "bubby"]
@@ -14,36 +13,35 @@ labels = ["panda", "milo", "bubby"]
 
 
 class Camera:
-	
-	cap = cv2.VideoCapture(0)
-	
-	
-	@classmethod
-	def tear_down(cls):
-		cls.cap.release()
-		cv2.destroyAllWindows()
+		
 	
 	@staticmethod
-	def capture_image(name:str ="bubby", cap=cap) -> None:
+	def capture_image(name:str ="bubby") -> None:
 		"""
 		
 		"""
+		cap = cv2.VideoCapture(0)
 		time.sleep(5)
 		ret, frame = cap.read()
 		MongoDBClient.add_image("ObjectDetection", Camera.convert_binary(frame), f"{name}_{uuid.uuid1()}")
 		# cv2.imwrite(f"{name}.jpg", frame)
 		# cv2.imshow("frame", frame)
 		time.sleep(2)
+		cap.release()
+		cv2.destroyAllWindows()
 		
 		
 	@staticmethod
-	def test_camera(cap=cap) -> None:
+	def test_camera() -> None:
+		cap = cv2.VideoCapture(0)
 		print("press q to quit")
 		while True:
 			ret, frame = cap.read()
 			cv2.imshow("frame", frame)
 			if cv2.waitKey(1) & 0xff == ord('q'):
 				break
+		cap.release()
+		cv2.destroyAllWindows()
 	 
 	@staticmethod 
 	def convert_binary(frame: np.ndarray) -> str:
@@ -54,6 +52,5 @@ class Camera:
 		nparr = np.fromstring(binary, np.uint8)
 		return cv2.imdecode(binary, cv2.CV_LOAD_IMAGE_COLOR)	
 	 	
-	 	
-atexit.register(Camera.tear_down)
+	 
 	 
